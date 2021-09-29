33847514fe58107fdeba6175fd189ec665289694
DixyNL <Steveteuling@outlook.com>
2021-09-29 17:47:08 +0200

    Quickfix car box colliders
    Hoogte van de boxcolliders aangepast, zodat ze niet meer net tussen de shelfs kunnen komen.
    
    ADDED:
    Summarys bij CollisionDetection.cs

--- Assets/Scripts/Lift/CollisionDetection.cs
+++ Assets/Scripts/Lift/CollisionDetection.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
-    // 
+    /// <summary>
+    /// OnTrigger check if the box collider from the scissor_lift touches any objects with the tag "Wall" or "Shelf"
+    /// If contact with said tagged object persists disallow movement in the respective destination
+    /// </summary>
 
+    /// makes sure the box collider is the front box collider
     [SerializeField]
     private bool isFront;
+    /// makes sure the box collider is the back box collider
     [SerializeField]
     private bool isBack;
+    /// <summary>
+    /// Allows the call to the ControllerMovementScript that passes the public bool for car movement
+    /// and dissalows if contact persists
+    /// </summary>
     [SerializeField] private ControllerMovementScript _controllerMovementScript;
+
+
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Shelf" || collider.gameObject.tag == "Wall")
