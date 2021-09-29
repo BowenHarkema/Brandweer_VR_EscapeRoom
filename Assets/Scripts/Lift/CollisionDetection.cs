8ed5b39741f89cfd9cfe71296acb9bffd086dc05
DixyNL <s1149715@student.windesheim.nl>
2021-09-29 10:36:49 +0200

    added collision
    cart collision en player collision with cart

---
+++ Assets/Scripts/Lift/CollisionDetection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    // 
+    [SerializeField]
+    private bool isFront;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Shelf")
+        {
+            if (isFront)
+            {
+                print("er is contact voor");
+            }
+            else
+            {
+                print("er is contact achter");
+            }
+            //freeze button front
+            
+        }
+        else
+        {
+            print("je kan gassen maat");
+        }
+    }
+}
