d7e5bf9eb51973f8282b8702ad0d994845a48487
DixyNL <Steveteuling@outlook.com>
2021-10-26 22:57:31 +0200

    Camera beeld op Screens
    screens laten de camera beelden van de cores zien

---
+++ Assets/Scripts/CoreRoom/ChangeCamera.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using System;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public UnityEvent onPressed, onReleased;
+
+    [SerializeField] private float threshold = 0.01f;
+    [SerializeField] private float deadZone = 0.025f;
+
+ 
+    [SerializeField] private bool _isPressed;
+    [SerializeField] private bool _SideCam;
+    [SerializeField] private bool _TopCam;
+    [SerializeField] private Vector3 _startPos;
+    [SerializeField] public GameObject _Canvas;
+    [SerializeField] private RenderTexture _Camera1;
+    [SerializeField] private RenderTexture _Camera2;
+    [SerializeField] private ConfigurableJoint _joint;
+    [SerializeField] private Rigidbody _rb;
+
+    void Start()
+    {
+ 
+        _startPos = transform.localPosition;
+        _joint = GetComponent<ConfigurableJoint>();
+
+    }
+
+    void Update()
+    {
+        if (!_isPressed && GetValue() + threshold >= 1)
+        {
+            Pressed();
+        }
+
+        if (_isPressed && GetValue() - threshold <= 0)
+        {
+
+
+            Released();
+        }
+
+    }
+
+    private float GetValue()
+    {
+        var value = Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
+
+        if (Math.Abs(value) < deadZone)
+            
+            value = 0;
+
+        return Mathf.Clamp(value, -1f, 1f);
+        
+    }
+
+    private void Pressed()
+    {
+        if(_SideCam == true)
+        {
+            _Canvas.GetComponent<ScreenSwitcher>().cam1 = true;
+            _Canvas.GetComponent<ScreenSwitcher>().cam2 = false;
+
+        }
+        if(_TopCam == true)
+        {
+            _Canvas.GetComponent<ScreenSwitcher>().cam2 = true;
+            _Canvas.GetComponent<ScreenSwitcher>().cam1 = false;
+
+        }
+
+    }
+
+    private void Released()
+    {
+        _isPressed = false;
+        
+        print("release button");
+
+    }
+ 
+    /*private void ChangeCam()
+    {
+        if(_SideCam ==true)
+        {
+            _Canvas.GetComponent<Renderer>().material.mainTexture = _Camera2;
+            print("pressed camerachange to SideCam");
+            
+        }
+        else
+        {
+            _Canvas.GetComponent<Renderer>().material.mainTexture = _Camera1;
+
+            print("pressed camerachange to TopCam");
+            
+        }
+        
+    }*/
+
+}
---
+++ Assets/Scripts/CoreRoom/ScreenSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSwitcher : MonoBehaviour
+{
+    [SerializeField] ChangeCamera changeCamera;
+
+    [SerializeField] public GameObject _Canvas;
+    [SerializeField] private RenderTexture _Camera1;
+    [SerializeField] private RenderTexture _Camera2;
+    [SerializeField] public bool cam1;
+    [SerializeField] public bool cam2;
+
+    // Start is called before the first frame update
+    public void ChangeCamOne()
+    {
+        
+        _Canvas.GetComponent<Renderer>().material.mainTexture = _Camera2;
+        
+        
+    }
+
+    public void ChangeCamTwo()
+    {
+        
+        _Canvas.GetComponent<Renderer>().material.mainTexture = _Camera1;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cam1 == true)
+        {
+            ChangeCamOne();
+        
+        }
+        if(cam2 == true)
+        {
+            ChangeCamTwo();
+
+        }
+      
+    }
+}
