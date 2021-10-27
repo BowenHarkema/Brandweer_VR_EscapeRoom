b0a5b48772d416735b5f1295ca20541ee217dd7b
BowenHarkema <b.harkema@clappform.com>
2021-10-27 14:49:47 +0200

    button largely done working on no collision when from under

---
+++ Assets/Scripts/Engine room/Panels/CamPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPanel : MonoBehaviour
+{
+    [SerializeField] public GameObject _Canvas;
+    [SerializeField] private RenderTexture _Camera1;
+    [SerializeField] private RenderTexture _Camera2;
+    [SerializeField] public bool Cam;
+
+    // Start is called before the first frame update
+    public void ChangeCam()
+    {
+        Cam = !Cam;
+
+        if(Cam)
+        {
+            _Canvas.GetComponent<Renderer>().material.mainTexture = _Camera2;
+        }
+        else
+        {
+            _Canvas.GetComponent<Renderer>().material.mainTexture = _Camera1;
+        }
+    }
+}
--- Assets/Scripts/Engine room/PhysicsButton.cs
+++ Assets/Scripts/Engine room/PhysicsButton.cs
@@ -6,49 +6,52 @@ using UnityEngine.Events;
 public class PhysicsButton : MonoBehaviour
 {
     [SerializeField]
-    public UnityEvent OnPressed, OnReleased;
+    public UnityEvent _OnPressed, _OnReleased;
 
     [SerializeField]
-    private float Threshold = .1f;
+    private float _Threshold = .1f;
 
     [SerializeField]
-    private float DeadZone = .025f;
+    private float _DeadZone = .025f;
 
     [SerializeField]
-    private bool IsPressed = false;
+    private bool _IsPressed = false;
 
     [SerializeField]
-    private bool _IsAlreadyPressed = false, _IsMainEngine = false; 
+    private bool _IsOneTimeButton = false;
 
-    [SerializeField]
     private Vector3 StartPos;
-
-    [SerializeField]
     private ConfigurableJoint Joint;
+    private bool _IsAlreadyPressed = false;
+    private Rigidbody _RigidBody;
 
+    //Set refs
     void Start()
     {
         StartPos = transform.localPosition;
         Joint = GetComponent<ConfigurableJoint>();
+        _RigidBody = gameObject.GetComponent<Rigidbody>();
     }
 
+    //Check if button is pressed
     void Update()
     {
-        if (!IsPressed && GetValue() + Threshold >= 1)
+        if (!_IsPressed && GetValue() + _Threshold >= 1)
         {
             Pressed();
         }
-        if (IsPressed && GetValue() - Threshold <= 0)
+        if (_IsPressed && GetValue() - _Threshold <= 0)
         {
             Released();
         }
     }
 
+    //Function to get bck 0 or 1
     private float GetValue()
     {
         var value = Vector3.Distance(StartPos, transform.localPosition) / Joint.linearLimit.limit;
 
-        if (Mathf.Abs(value) < DeadZone)
+        if (Mathf.Abs(value) < _DeadZone)
         {
             value = 0;
         }
@@ -56,28 +59,30 @@ public class PhysicsButton : MonoBehaviour
         return Mathf.Clamp(value, -1f, 1f);
     }
 
+    //Pressed function
     private void Pressed()
     {
-        if(_IsMainEngine)
+        if (_IsOneTimeButton)
         {
-            if(!_IsAlreadyPressed)
+            if (!_IsAlreadyPressed)
             {
-                IsPressed = true;
+                _IsPressed = true;
+                _OnPressed.Invoke();
                 _IsAlreadyPressed = true;
-                OnPressed.Invoke();
             }
         }
         else
         {
-            IsPressed = true;
-            OnPressed.Invoke();
+            _IsPressed = true;
+            _OnPressed.Invoke();
         }
     }
 
+    //Released function
     private void Released()
     {
-        IsPressed = false;
+        _IsPressed = false;
         _IsAlreadyPressed = false;
-        OnReleased.Invoke();
+        _OnReleased.Invoke();
     }
 }
