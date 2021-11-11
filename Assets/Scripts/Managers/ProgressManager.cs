f81e24f89194a68cabfce3c44ecb1bc5546e5379
TimvdMeulen <vandermeulentim47@gmail.com>
2021-11-11 14:27:47 +0100

    Progressmanager custom property

--- Assets/Scripts/Managers/ProgressManager.cs
+++ Assets/Scripts/Managers/ProgressManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ProgressManager : MonoBehaviour
 {
@@ -9,11 +10,18 @@ public class ProgressManager : MonoBehaviour
     [SerializeField] private bool _AllGeneratorsUp;
     [SerializeField] private bool _AllPodsBalanced;
 
+    [SerializeField] private ExitGames.Client.Photon.Hashtable _MyCustomProperties = new ExitGames.Client.Photon.Hashtable();
+
     // Update is called once per frame
     void Update()
     {
         _AllGeneratorsUp = _GeneratorSync.GeneratorUpCount == 5 ? true : false;
         _AllPodsBalanced = _PlantPodsController.getBrokenPods() == 0 ? true : false;
-        //if bools are true call events to happen
+
+        _MyCustomProperties["Generators"] = _AllGeneratorsUp;
+        _MyCustomProperties["PlantPods"] = _AllGeneratorsUp;
+        PhotonNetwork.LocalPlayer.CustomProperties = _MyCustomProperties;
+
+        //check if customproperty is true, then call event
     }
 }
