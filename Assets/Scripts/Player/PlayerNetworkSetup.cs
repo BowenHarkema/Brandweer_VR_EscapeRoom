e18f205589e00a9f7247046e88393396a1c61ef7
TimvdMeulen <vandermeulentim47@gmail.com>
2021-12-03 13:30:46 +0100

    player colours

--- Assets/Scripts/Player/PlayerNetworkSetup.cs
+++ Assets/Scripts/Player/PlayerNetworkSetup.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.XR.Interaction.Toolkit;
-
+using System;
 public class PlayerNetworkSetup : MonoBehaviourPunCallbacks
 {
     public GameObject localXRig;
 
     public GameObject avatarHead;
     public GameObject avatarBody;
+    public GameObject avatarHair;
     // Start is called before the first frame update
+
+    private Color[] _Colors = { Color.blue, Color.black, Color.yellow, Color.red, Color.green };
+    private int[] _OwnerActorNrs = new int[4];
+    private static int _ArrayCounter;
     void Start()
     {
         if (photonView.IsMine)
         {
             localXRig.SetActive(true);
             gameObject.GetComponent<AvatarInputConverter>().enabled = true;
+
+            _OwnerActorNrs[_ArrayCounter] = photonView.OwnerActorNr;
+            _ArrayCounter++;
+
+            avatarHair.GetComponent<MeshRenderer>().material.color = _Colors[Array.FindIndex(_OwnerActorNrs, x => x == photonView.OwnerActorNr)];
+
             SetLayerRecursively(avatarHead,10);
             SetLayerRecursively(avatarBody,11);
 
@@ -34,15 +45,16 @@ public class PlayerNetworkSetup : MonoBehaviourPunCallbacks
         {
             localXRig.SetActive(false);
             gameObject.GetComponent<AvatarInputConverter>().enabled = false;
+
+            //sets the colour of the player head/hair
+            _OwnerActorNrs[_ArrayCounter] = photonView.OwnerActorNr;
+            _ArrayCounter++;
+
+            avatarHair.GetComponent<MeshRenderer>().material.color = _Colors[Array.FindIndex(_OwnerActorNrs, x => x == photonView.OwnerActorNr)];
+
             SetLayerRecursively(avatarHead, 0);
             SetLayerRecursively(avatarBody, 0);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     void SetLayerRecursively(GameObject go, int layerNumber)
     {
